c92c20c264344916a583977c81737ae01e9d8417
komal <you@example.com>
2017-03-31 17:28:07 +0530

    first commit

---
+++ tests/acceptance/loginCept.php
@@ -0,0 +1,19 @@
+<?php
+  use Codeception\Util\Fixtures;
+  use lib\BrowserStackWebDriver;
+
+  // @group single
+  $I = new AcceptanceTester($scenario);
+  $I-> wantTo('ensure that user is sucessfully logged in');
+  $I-> amOnPage('/login');
+  $I-> fillField('Username', 'sys_admin');
+  $I-> fillField('Password',  'p@ssw0rd1');
+  $I-> click('Sign in');
+  $I-> see('Patients' , 'span');
+  $I-> seeInCurrentUrl('/patients');
+  $I-> seeLink('Register New Patient');
+  $I-> click('Register New Patient');
+  $I->makeScreenshot('edit_page');
+  $I-> canSeeInCurrentUrl("patients/new");
+  $I-> see('Patient Pre-Registration Form' , 'span#pre-registration-header');
+  
